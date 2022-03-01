57dea584c80cbb7978b34c2aab0a1d99542abb16
Guilherme Haetinger <ghaetinger@gmail.com>
2022-03-01 01:29:49 +0000

    :recycle: refactored d3 component structure

--- src/D3Components.jl
+++ src/D3Components.jl
@@ -15,7 +15,7 @@ macro bind(def, element)
 end
 
 # ╔═╡ 774bb4f2-963e-11ec-30ff-e3394f52858b
-using HypertextLiteral, PlutoUI
+using HypertextLiteral, PlutoUI, Parameters
 
 # ╔═╡ 0c520e27-04d5-47ff-814c-51c9dedd77e3
 function skip_as_script(m::Module)
@@ -33,6 +33,12 @@ macro skip_as_script(ex) skip_as_script(__module__) ? esc(ex) : nothing end
 md"""
 Need to implement js mime for components and test around to see what works
 """
+
+# ╔═╡ aafd3c10-c0e5-48ab-81e4-b49430c5cc11
+md"# Random things"
+
+# ╔═╡ 52578854-40af-4af7-95e9-06d011615524
+randstring() = String(rand(collect('a':'z'), 10))
 
 # ╔═╡ 09e4ee53-b8b7-4de0-9439-d1bc7bc22578
 md"# JS loose script"
@@ -52,7 +58,7 @@ begin
 	function Base.show(io::IO, m::MIME"text/javascript", r::RenderWithoutScriptTags)
 		full_result = repr(MIME"text/html"(), r.x; context=io)
 		write(io, 
-			"(() => {" * full_result[1+length("<script>"):end-length("</script>")] * "})()"
+			"((id=0) => {" * full_result[1+length("<script>"):end-length("</script>")] * "})"
 		)
 	end
 end
@@ -88,6 +94,15 @@ attr_style_to_javascript(head::String, ls::Dict{String, String}) =
 # ╔═╡ 5b83dab0-2c5f-47d1-9a6b-1915c84b9d6c
 abstract type D3Component end
 
+# ╔═╡ 9fc986af-5532-4474-85d6-0d19399237af
+begin
+  struct D3ComponentID
+    id :: String
+  end
+
+  D3ComponentID() = D3ComponentID(randstring())
+end;
+
 # ╔═╡ d23077cf-621f-41c8-a7c7-80b33e6e3c70
 md"## Linear Scale" 
 
@@ -98,12 +113,10 @@ begin
     range  :: Tuple{T, T}
   end
 	
-  function LinearScale(values::Vector{T}) where T <: Real
+  function LinearScale(values::Vector{T}, range) where T <: Real
     domain = extrema(values)
 	domain_type = domain |> first |> typeof
-  	# This has to be related to Canvas size
-    range  = [0, 100] .|> domain_type |> Tuple 
-	LinearScale(domain, range)
+	LinearScale(domain, range |> Tuple .|> domain_type)
   end
 end;
 
@@ -116,106 +129,129 @@ show(io, m, @js_str("""
 """))
 end
 
+# ╔═╡ 9099c1e2-aa63-4b87-85f3-408677e7cbad
+md"## SVG Component Types"
+
+# ╔═╡ c8ba9d49-25d1-4e52-a372-b4d3e284f769
+begin
+  @enum SVGComponent Path Rect Circle
+  Base.show(io::IO,  m::MIME"text/javascript", svg::SVGComponent) = Base.show(io, m, svg |> Symbol |> string |> lowercase |> HypertextLiteral.JavaScript)
+end
+
+# ╔═╡ 793e2198-560d-461c-a9c4-40e042eab790
+md"## D3 Attributes"
+
+# ╔═╡ c80d5063-aac3-4ee7-994c-eb6394b225eb
+@with_kw struct D3Attributes
+  attributes    :: Dict{String, String} = Dict()
+  style         :: Dict{String, String} = Dict()
+  animationTime :: Int = 200
+end
+
 # ╔═╡ eb38a550-4002-45e7-804f-e3cc895cd4ed
 md"## Line"
 
 # ╔═╡ 993aeb91-18ce-4a36-87de-fb0387caa8d1
-struct Line <: D3Component
+begin
+  struct Line <: D3Component
+    data   :: Vector{NamedTuple{(:x, :y), Tuple{<:Real, <:Real}}}
 	scaleX :: LinearScale
 	scaleY :: LinearScale
+	svgComponent :: SVGComponent
+	d3Attributes :: D3Attributes
+  end
+
+  Line(x, y;
+        cwidth=100,
+        cheight=100,
+        offset=0,
+        svgComponent=Path,
+        d3Attributes=D3Attributes()
+  ) = Line(
+	  [(x=x[i], y=y[i]) for i ∈ 1:length(x)],
+	  LinearScale(x, [0, cwidth] .+ offset),
+	  LinearScale(y, [0, cheight] .+ offset),
+	  svgComponent,
+	  d3Attributes
+  )
 end
 
 # ╔═╡ fc3b096e-18b3-4536-b20d-753e3ddf7082
 Base.show(io::IO, m::MIME"text/javascript", line::Line) =
 show(io, m, @js_str """
-    const xScale = $(line.scaleX);
-    const yScale = $(line.scaleY);
-    return d3.line()
+    const xScale = $(line.scaleX)();
+    const yScale = $(line.scaleY)();
+    const path = d3.line()
       .x(d => xScale(d.x))
       .y(d => yScale(d.y))
       .curve(d3.curveNatural)
-""")
 
-# ╔═╡ 574cb7d0-0c19-4ff3-b8f6-c3b2c3696e0d
-md"## Line Path"
-
-# ╔═╡ 01134a40-007c-443f-b693-67507c3cf636
-abstract type Path <: D3Component end
-
-# ╔═╡ 8b2b6a92-c48c-4645-a42f-88007af05c88
-struct LinePath <: Path
-	attributes :: Dict{String, String}
-	style      :: Dict{String, String}
-	x          :: Vector{Real}
-	y          :: Vector{Real}
-	line       :: Line
-end
-
-# ╔═╡ c6d24eb0-2fff-489d-8fdd-22348eef07ec
-Base.show(io::IO, m::MIME"text/javascript", line_path::LinePath) = 
-show(io, m, @js_str """
-    const line = $(line_path.line);
-	const data = 
-		$([(x=line_path.x[i], y=line_path.y[i]) for i ∈ 1:length(line_path.x)]);
-    s.selectAll(".line")
+	const data = $(line.data);
+    s.selectAll(".line-" + id)
 	 .data([data])
-     .join("path")
+     .join("$(line.svgComponent)")
      .transition()
 	 .duration(200)
-     .attr("d", line)
-     $(attr_style_to_javascript("attr", line_path.attributes))
-     $(attr_style_to_javascript("style", line_path.style))
-    return s
+     .attr("d", path)
+     .attr("class", "line-" + id)
+     $(line.d3Attributes)
 """)
+
+# ╔═╡ d544a9cc-4412-4b7d-a223-5bb181b595bb
+Base.show(io::IO, m::MIME"text/javascript", d3attrs::D3Attributes) =
+	Base.show(io, m, 
+		attr_style_to_javascript("attr", d3attrs.attributes)
+	)
 
 # ╔═╡ 5a9b9cd0-afd9-4d41-9783-5488b0da75f2
 md"# D3 Canvas"
 
-# ╔═╡ 58dd9b4e-f606-4431-858b-aaa5c4f906cf
-# function Base.show(io::IO, m::MIME"text/javascript", components)
-# 	@info "ASDASD"
-# 	for component ∈ components
-# 		show(io, m, component)
-# 		write(io, ";")
-# 	end
-# end
-
 # ╔═╡ 91f71b8a-c135-4d8c-8e86-c2aeb48ece30
 begin
-  mutable struct D3Canvas
-    components :: Vector{Any}
-    width 	   :: Int64
-    height     :: Int64
-	id         :: Float64
+  struct D3Canvas
+    attributes    :: Dict{String, String}
+    style         :: Dict{String, String}
+	width 	      :: Int64
+    height        :: Int64
+	id            :: String
   end
-  function D3Canvas(width, height)
-	  D3Canvas([], width, height, rand())
+  function D3Canvas(attrs, style, width, height)
+	  D3Canvas(attrs, style, width, height, randstring())
   end
-end
+end;
 
-# ╔═╡ 2b3aa868-b257-4a55-9ea6-060b73fe1eb9
-Base.show(io::IO, m::MIME"text/html", canvas::D3Canvas) = 
-show(io, m, @htl """
+# ╔═╡ 501395c5-81db-493b-8e5c-1fdf8c2d86ab
+md"## Paint function"
+
+# ╔═╡ 9cd3619a-d3cd-4bf9-b0cf-c93b25d6ff9e
+function paint(canvas::D3Canvas, components::Vector{<:D3Component})
+@htl """
 <script src="https://cdn.jsdelivr.net/npm/d3@6.2.0/dist/d3.min.js"></script>
 <script id="$(canvas.id)">
 	const svg = this == null ? DOM.svg($(canvas.width),$(canvas.height)) : this;
-	var s = this == null ? d3.select(svg) : this.s;
+	const s = this == null ? d3.select(svg) : this.s;
 
-	$(canvas.components);
+	var comp_foos = $components;
+
+	for (var i = 0; i < comp_foos.length; i++) {
+		comp_foos[i](i);
+	}
 
 	const output = svg
 	output.s = s
 	return output
 </script>
-""")
-
-# ╔═╡ 501395c5-81db-493b-8e5c-1fdf8c2d86ab
-function set_components!(d3Canvas::D3Canvas, components::Vector{}) 
-	d3Canvas.components = [components]
+"""
 end
 
 # ╔═╡ b894890a-dab0-4f39-984d-bd37c93d470b
 md"## Example"
+
+# ╔═╡ ffbc5a8f-dd1b-455c-8005-3589442dab63
+@bind cwidth Slider(100:1000)
+
+# ╔═╡ dcc56a7d-e070-4f4f-a571-1bb7fe981cdd
+@bind cheight Slider(100:1000)
 
 # ╔═╡ fa58813a-abdf-4ca2-965a-6b600fbd85c6
 @skip_as_script x = collect(1:100)
@@ -223,63 +259,35 @@ md"## Example"
 # ╔═╡ dcdd477b-729c-4e34-9da8-19d702ee7920
 @skip_as_script y = [rand() for _ ∈ 1:100]
 
-# ╔═╡ fcfc4312-daee-40e0-ae51-f77fa84c0eda
-@skip_as_script x_lscale = LinearScale(x)
+# ╔═╡ 06924b38-26df-47fd-98c5-838b623e9039
+canvas = D3Canvas(Dict(), Dict(), 1000, 1000)
 
-# ╔═╡ d33fed69-809a-416f-92b0-4110eb783015
-@skip_as_script y_lscale = LinearScale(y)
-
-# ╔═╡ 80ba2870-2018-4dab-ac2e-e71f47cb9671
-@skip_as_script line = Line(x_lscale, y_lscale)
-
-# ╔═╡ e479cb18-7486-4ec3-a0f3-b0274eb5e1b7
-@skip_as_script attributes = Dict(
-        "fill" => "none",
-        "stroke" => "black",
-        "stroke-width" => "0.5",
-        "class" => "line"
-)
-
-# ╔═╡ bbaa40af-b76b-421e-b556-df5dd4c7f925
-@skip_as_script second_attributes = Dict(
-        "fill" => "none",
-        "stroke" => "pink",
-        "stroke-width" => "2.5",
-        "class" => "line"
-)
-
-# ╔═╡ 2144cbf4-75ac-4781-af0c-e116c65b5647
-@skip_as_script second_line_path = LinePath(second_attributes, Dict(), x, y, line)
-
-# ╔═╡ 198a3eb5-f86b-48f4-ac7a-d36d1c943a50
-@skip_as_script line_path = LinePath(attributes, Dict(), x, y, line)
-
-# ╔═╡ 0c20439f-2d84-4c54-9729-3232e69a3cd7
-@bind dims PlutoUI.combine() do Child
-	md"""
-	Width: $(Child(:width, PlutoUI.Slider(100:10:1000, show_value=true)))
-	
-	Height: $(Child(:height, PlutoUI.Slider(100:10:1000, show_value=true)))
-	"""
-end;
-
-# ╔═╡ 55b1177b-7733-4ba9-9de2-21a1ace9872b
-@skip_as_script canvas = D3Canvas(100, 100)
-
-# ╔═╡ 4727ec02-eb3e-42b4-b2b1-21e0cd8e1bcc
-set_components!(canvas, [second_line_path, line_path])
-
-# ╔═╡ 89f913d9-e024-46dc-a593-fd219bc95bc1
-canvas
+# ╔═╡ d4bd2b90-9450-4c51-993d-ee0ba75d0167
+paint(canvas, [
+	Line(x, y;
+	cwidth=cwidth,
+	cheight=cheight,
+	d3Attributes=D3Attributes(
+		attributes=Dict("stroke" => "pink", "fill" => "none", "stroke-width" => "2.5")
+	)),
+Line(x, y;
+	cwidth=cwidth,
+	cheight=cheight,
+	d3Attributes=D3Attributes(
+		attributes=Dict("stroke" => "black", "fill" => "none")
+))
+])
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
 HypertextLiteral = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+Parameters = "d96e819e-fc66-5662-9728-84c9c7592b0a"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
 
 [compat]
 HypertextLiteral = "~0.9.3"
+Parameters = "~0.12.3"
 PlutoUI = "~0.7.35"
 """
 
@@ -287,7 +295,7 @@ PlutoUI = "~0.7.35"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.1"
+julia_version = "1.7.2"
 manifest_format = "2.0"
 
 [[deps.AbstractPlutoDingetjes]]
@@ -403,6 +411,17 @@ uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
 
+[[deps.OrderedCollections]]
+git-tree-sha1 = "85f8e6578bf1f9ee0d11e7bb1b1456435479d47c"
+uuid = "bac558e1-5e72-5ebc-8fee-abe8a469f55d"
+version = "1.4.1"
+
+[[deps.Parameters]]
+deps = ["OrderedCollections", "UnPack"]
+git-tree-sha1 = "34c0e9ad262e5f7fc75b10a9952ca7692cfc5fbe"
+uuid = "d96e819e-fc66-5662-9728-84c9c7592b0a"
+version = "0.12.3"
+
 [[deps.Parsers]]
 deps = ["Dates"]
 git-tree-sha1 = "13468f237353112a01b2d6b32f3d0f80219944aa"
@@ -469,6 +488,11 @@ uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 deps = ["Random", "SHA"]
 uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
 
+[[deps.UnPack]]
+git-tree-sha1 = "387c1f73762231e86e0c9c5443ce3b4a0a9a0c2b"
+uuid = "3a884ed6-31ef-47d7-9d2a-63182c4928ed"
+version = "1.0.2"
+
 [[deps.Unicode]]
 uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 
@@ -494,6 +518,8 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╟─ee7cc0db-3b79-4099-b566-b673fb3744a8
 # ╠═774bb4f2-963e-11ec-30ff-e3394f52858b
 # ╠═68e6b1f4-b0c3-4fbf-905f-454b6bf0cb8f
+# ╟─aafd3c10-c0e5-48ab-81e4-b49430c5cc11
+# ╟─52578854-40af-4af7-95e9-06d011615524
 # ╟─09e4ee53-b8b7-4de0-9439-d1bc7bc22578
 # ╠═4d193bff-9ed2-42ba-9cef-4d8c9abc8332
 # ╠═c9458743-0471-430e-b7a5-e26a2ccb5811
@@ -502,34 +528,28 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╟─e3156da9-8603-41f9-ad41-03eaf79c4540
 # ╠═44721f68-c05e-4d6d-a721-6809a4ab72f8
 # ╠═5b83dab0-2c5f-47d1-9a6b-1915c84b9d6c
+# ╠═9fc986af-5532-4474-85d6-0d19399237af
 # ╟─d23077cf-621f-41c8-a7c7-80b33e6e3c70
 # ╠═348c15a1-1f65-4827-8d30-f636dff6b9dd
 # ╠═d63f33e2-0892-499c-9dc6-9a017109cc54
+# ╟─9099c1e2-aa63-4b87-85f3-408677e7cbad
+# ╠═c8ba9d49-25d1-4e52-a372-b4d3e284f769
+# ╟─793e2198-560d-461c-a9c4-40e042eab790
+# ╠═c80d5063-aac3-4ee7-994c-eb6394b225eb
+# ╠═d544a9cc-4412-4b7d-a223-5bb181b595bb
 # ╟─eb38a550-4002-45e7-804f-e3cc895cd4ed
 # ╠═993aeb91-18ce-4a36-87de-fb0387caa8d1
 # ╠═fc3b096e-18b3-4536-b20d-753e3ddf7082
-# ╟─574cb7d0-0c19-4ff3-b8f6-c3b2c3696e0d
-# ╠═01134a40-007c-443f-b693-67507c3cf636
-# ╠═8b2b6a92-c48c-4645-a42f-88007af05c88
-# ╠═c6d24eb0-2fff-489d-8fdd-22348eef07ec
 # ╟─5a9b9cd0-afd9-4d41-9783-5488b0da75f2
-# ╠═58dd9b4e-f606-4431-858b-aaa5c4f906cf
 # ╠═91f71b8a-c135-4d8c-8e86-c2aeb48ece30
-# ╠═2b3aa868-b257-4a55-9ea6-060b73fe1eb9
-# ╠═501395c5-81db-493b-8e5c-1fdf8c2d86ab
+# ╟─501395c5-81db-493b-8e5c-1fdf8c2d86ab
+# ╠═9cd3619a-d3cd-4bf9-b0cf-c93b25d6ff9e
 # ╟─b894890a-dab0-4f39-984d-bd37c93d470b
-# ╟─fa58813a-abdf-4ca2-965a-6b600fbd85c6
+# ╠═ffbc5a8f-dd1b-455c-8005-3589442dab63
+# ╠═dcc56a7d-e070-4f4f-a571-1bb7fe981cdd
+# ╠═fa58813a-abdf-4ca2-965a-6b600fbd85c6
 # ╠═dcdd477b-729c-4e34-9da8-19d702ee7920
-# ╟─fcfc4312-daee-40e0-ae51-f77fa84c0eda
-# ╟─d33fed69-809a-416f-92b0-4110eb783015
-# ╟─80ba2870-2018-4dab-ac2e-e71f47cb9671
-# ╟─e479cb18-7486-4ec3-a0f3-b0274eb5e1b7
-# ╟─bbaa40af-b76b-421e-b556-df5dd4c7f925
-# ╠═2144cbf4-75ac-4781-af0c-e116c65b5647
-# ╠═198a3eb5-f86b-48f4-ac7a-d36d1c943a50
-# ╠═0c20439f-2d84-4c54-9729-3232e69a3cd7
-# ╠═55b1177b-7733-4ba9-9de2-21a1ace9872b
-# ╠═4727ec02-eb3e-42b4-b2b1-21e0cd8e1bcc
-# ╠═89f913d9-e024-46dc-a593-fd219bc95bc1
+# ╠═d4bd2b90-9450-4c51-993d-ee0ba75d0167
+# ╠═06924b38-26df-47fd-98c5-838b623e9039
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
