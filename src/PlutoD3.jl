bd6a83a4aa408b026c2c01885aeb767df9f52da4
Guilherme Haetinger <ghaetinger@gmail.com>
2022-03-14 12:51:48 -0300

    :sparkles: Shape function!

--- src/PlutoD3.jl
+++ src/PlutoD3.jl
@@ -2,12 +2,16 @@ module PlutoD3
 
 using Reexport
 
-export Area, D3Attributes, D3Component, D3Canvas, Line, LinearScale, @js
+export Area, D3Attributes, D3Component, D3Canvas, Line, LinearScale, @js, Shape
 
 include("./d3_abstractions.jl")
+
 include("./canvas.jl")
+
 include("./linear_scale.jl")
-include("./line.jl")
+
 include("./area.jl")
+include("./line.jl")
+include("./shape.jl")
 
 end # module
--- test/example2.jl
+++ test/example2.jl
@@ -33,10 +33,13 @@ end
 end
 
 # ╔═╡ cc8b977d-079a-4124-b9e2-c3d329cc2cd5
-x = collect(1:100);
+x = collect(1:50);
 
 # ╔═╡ 8b7a8f64-05b2-422b-8ec5-52da5d8acb68
 y = [rand() for _ ∈ x];
+
+# ╔═╡ dc81bcdd-df79-4f23-82fc-6efefc299d46
+size = [rand() * 100 for _ ∈ x];
 
 # ╔═╡ 74185d5a-4c10-4570-a99d-44b7bed8afae
 D3Canvas([
@@ -51,6 +54,17 @@ D3Canvas([
 					"stroke-width" => "$(param.strw)"
 				)
 			)
+		),
+		Shape(x, y, size;
+		cwidth=param.width,
+			cheight=param.height,
+		    offset = 50,
+			d3Attributes=D3Attributes(;
+				attributes=Dict(
+				"fill" => "none",
+				"stroke" => "black"
+				)
+			)
 		)
 	], 
 	"my-canvas";
@@ -61,8 +75,9 @@ D3Canvas([
 )
 
 # ╔═╡ Cell order:
-# ╟─a766285c-9b11-11ec-0609-1b79637175d3
+# ╠═a766285c-9b11-11ec-0609-1b79637175d3
 # ╟─39bd54a5-6f73-43ff-b5ec-90551642741d
 # ╠═cc8b977d-079a-4124-b9e2-c3d329cc2cd5
 # ╠═8b7a8f64-05b2-422b-8ec5-52da5d8acb68
+# ╠═dc81bcdd-df79-4f23-82fc-6efefc299d46
 # ╠═74185d5a-4c10-4570-a99d-44b7bed8afae
